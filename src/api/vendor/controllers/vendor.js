f553cb131f3149118ad74a5afadc90deac1b2f73
Arjun Puri <99791938+nameIsArjun@users.noreply.github.com>
2022-11-08 13:34:26 -0500

    feat/theme page

--- src/api/vendor/controllers/vendor.js
+++ src/api/vendor/controllers/vendor.js
@@ -5,9 +5,10 @@
  */
 
 // const createPopulatedController = require("../../../helpers/populate");
-// module.exports = createPopulatedController('api::vendor.vendor');
+const { createCoreController } = require('@strapi/strapi').factories;
+module.exports = createCoreController('api::vendor.vendor');
 
-const schema = require("../content-types/vendor/schema.json");
-const createPopulatedController = require("../../../helpers/populate");
+// const schema = require("../content-types/vendor/schema.json");
+// const createPopulatedController = require("../../../helpers/populate");
 
-module.exports = createPopulatedController("api::vendor.vendor", schema);
+// module.exports = createPopulatedController("api::vendor.vendor", schema);
